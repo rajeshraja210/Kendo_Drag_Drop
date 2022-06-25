1cdbfff3152e5d8dfbb5fd803734fbb5ae4df29a
rajesh <rajeshraja210@users.noreply.github.com>
2022-06-25 13:43:15 +0530

    Initial commit
    Initial commit kendo app

---
+++ KendoMVC/KendoMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace KendoMVC
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+           
+            bundles.Add(new ScriptBundle("~/bundles/Kendo/js").Include(
+                    "~/Kendo/js/jquery.min.js",
+                    "~/Kendo/js/kendo.all.min.js",
+                    "~/Kendo/js/kendo.aspnetmvc.min.js"
+                ));
+           
+            bundles.Add(new StyleBundle("~/Kendo/styles").Include("~/Kendo/styles/kendo.common.min.css",
+               "~/Kendo/styles/kendo.default.min.css"));
+           
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            //bundles.IgnoreList.Clear();
+        }
+    }
+}
---
+++ KendoMVC/KendoMVC/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using KendoMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult EmployeeList([DataSourceRequest] DataSourceRequest request)
+        {
+            try
+            {
+                List<Employee> _emp = new List<Employee>();
+                _emp.Add(new Employee(1, "apple", "Ross1"));
+                _emp.Add(new Employee(2, "ball", "Raj2"));
+                _emp.Add(new Employee(3, "cat", "Kumar"));
+                _emp.Add(new Employee(4, "doll", "Ross"));
+                _emp.Add(new Employee(5, "elephant", "Raj"));
+                _emp.Add(new Employee(6, "fail", "Kumar"));
+                _emp.Add(new Employee(7, "gap", "Ross"));
+                _emp.Add(new Employee(8, "hocky", "Raj"));
+                _emp.Add(new Employee(9, "india", "Kumar"));
+                _emp.Add(new Employee(10, "jail", "Ross"));
+                _emp.Add(new Employee(11, "kolor", "Raj"));
+                _emp.Add(new Employee(12, "loan", "Kumar"));
+                DataSourceResult result = _emp.ToDataSourceResult(request);
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.Message, JsonRequestBehavior.AllowGet);
+
+            }
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
